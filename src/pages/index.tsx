6e12f6eb475551fa2e920c2604a56de0ea67bf03
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-05-04 20:49:05 +0900

    💁‍♀️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import useSWR from "swr";
 
 const API_ENDPOINT = "https://api.github.com/users";
 
-const people = [
+const languages = [
   { id: 1, name: "TypeScript", unavailable: true },
   { id: 2, name: "JavaScript", unavailable: false },
   { id: 3, name: "HTML", unavailable: false },
@@ -22,17 +22,37 @@ const people = [
 const fetcher = (args) => fetch(args).then((res) => res.json());
 
 const Home = () => {
-  const [selected, setSelected] = useState(people[0]);
+  const [selected, setSelected] = useState(languages[0]);
   const [page, setPage] = useState<number | null>(1);
   const [user, setUser] = useState<string | null>("kotobuki562");
   const [query, setQuery] = useState<string | null>("TypeScript");
+  const [info, setInfo] = useState<any[] | null>([]);
+  const [error, setError] = useState<Error | null>();
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const { data, error } = useSWR(
-    `${API_ENDPOINT}/${user}/starred?page=${page}&per_page=100`,
-    fetcher
-  );
+  // const { data, error } = useSWR(
+  //   `${API_ENDPOINT}/${user}/starred?page=${page}&per_page=100`,
+  //   fetcher
+  // );
 
-  const filterLanguage = data?.filter((git) =>
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `${API_ENDPOINT}/${user}/starred?page=${page}&per_page=100`
+      );
+      const data = await res.json();
+      console.log(data);
+      setInfo(data);
+      setError(error);
+      return setLoading(false);
+    } catch (error) {
+      console.log(error);
+      return setLoading(false);
+    }
+  };
+
+  const filterLanguage = info?.filter((git) =>
     git.language.includes(selected.name)
   );
 
@@ -45,13 +65,6 @@ const Home = () => {
       <Layout>
         <div>
           <p>APIのリクエストの制限が超えました。</p>
-          <p>
-            詳しくは
-            <a className="text-teal-400" href={error?.documentation_url}>
-              こちら
-            </a>
-            をご覧ください
-          </p>
           <div className="w-72 top-16">
             <Listbox value={selected} onChange={setSelected}>
               {({ open }) => (
@@ -76,7 +89,7 @@ const Home = () => {
                       static
                       className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
                     >
-                      {people.map((person, personIdx) => (
+                      {languages.map((person, personIdx) => (
                         <Listbox.Option
                           key={personIdx}
                           className={({ active }) =>
@@ -146,10 +159,121 @@ const Home = () => {
       </Layout>
     );
   }
-  if (!filterLanguage) {
+  if (loading) {
     return (
       <Layout>
         <div className="w-full">
+          <div className="p-8">
+            <div className="transition duration-200 bg-white flex items-center rounded-full shadow-md hover:shadow-lg">
+              <input
+                className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
+                id="search"
+                type="text"
+                value={user}
+                onChange={(e) => setUser(e.target.value)}
+                placeholder="Your GitHub ID"
+              />
+              <div className="p-4">
+                <button
+                  onClick={() => getData()}
+                  className="bg-teal-500 text-white rounded-full p-2 hover:bg-teal-400 focus:outline-none w-12 h-12 flex items-center justify-center"
+                >
+                  <FaUserAlt />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full mb-8">
+            <Listbox value={selected} onChange={setSelected}>
+              {({ open }) => (
+                <div className="relative mt-1">
+                  <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+                    <span className="block truncate">{selected.name}</span>
+                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                      <HiSelector
+                        className="w-5 h-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options
+                      static
+                      className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                    >
+                      {languages.map((person, personIdx) => (
+                        <Listbox.Option
+                          key={personIdx}
+                          className={({ active }) =>
+                            `${
+                              active
+                                ? `text-${person.name} bg-amber-100`
+                                : `text-${person.name}`
+                            }
+                          cursor-default select-none relative py-2 pl-10 pr-4`
+                          }
+                          value={person}
+                        >
+                          {({ selected, active }) => (
+                            <>
+                              <span
+                                className={`${
+                                  selected
+                                    ? `font-medium text-${person.name}`
+                                    : `font-normal text-${person.name}`
+                                } block truncate`}
+                              >
+                                {person.name}
+                              </span>
+                              {selected ? (
+                                <span
+                                  className={`${
+                                    active
+                                      ? `text-${person.name}`
+                                      : `text-${person.name}`
+                                  }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                                >
+                                  <FaCheck
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              )}
+            </Listbox>
+          </div>
+
+          <div className="w-full flex items-center justify-around">
+            <Button
+              btnText="前へ"
+              useage={page === 1 ? null : "base"}
+              size="md"
+              onClick={() => setPage(page - 1)}
+              disabled={page === 1}
+            />
+            <Button
+              btnText="次へ"
+              useage={filterLanguage.length < 100 ? null : "base"}
+              size="md"
+              disabled={filterLanguage.length < 100}
+              onClick={() => setPage(page + 1)}
+            />
+          </div>
           <Skeleton />
           <Skeleton />
           <Skeleton />
@@ -175,15 +299,18 @@ const Home = () => {
               onChange={(e) => setUser(e.target.value)}
               placeholder="Your GitHub ID"
             />
-            <div className="p-4">
-              <button className="bg-teal-500 text-white rounded-full p-2 hover:bg-teal-400 focus:outline-none w-12 h-12 flex items-center justify-center">
+            <div className="py-2 px-4">
+              <button
+                onClick={() => getData()}
+                className="bg-teal-500 text-white rounded-full p-2 hover:bg-teal-400 focus:outline-none w-12 h-12 flex items-center justify-center"
+              >
                 <FaUserAlt />
               </button>
             </div>
           </div>
         </div>
 
-        <div className="w-full mb-8">
+        <div className="w-full mb-8 px-8">
           <Listbox value={selected} onChange={setSelected}>
             {({ open }) => (
               <div className="relative mt-1">
@@ -207,7 +334,7 @@ const Home = () => {
                     static
                     className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
                   >
-                    {people.map((person, personIdx) => (
+                    {languages.map((person, personIdx) => (
                       <Listbox.Option
                         key={personIdx}
                         className={({ active }) =>
